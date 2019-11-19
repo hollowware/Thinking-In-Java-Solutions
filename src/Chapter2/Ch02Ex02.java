11dbbc0b7c0b0346b686094c7aa4bc7ca1842944
Arthur <artur.muchliado@gmail.com>
2019-11-19 11:44:36 +0200

    Some Changes

---
+++ src/Chapter2/Ch02Ex02.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Chapter2;
+
+/**
+ *
+ * @author IX Hero
+ */
+public class Ch02Ex02 {
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World.");
+        
+    }
+}
